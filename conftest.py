bda03e87a91d2fe26ff2bf35c36a364901a52f6c
Rusakova <wolfik2k@gmail.com>
2022-06-19 23:06:09 +0400

    23-24 задание

--- conftest.py
+++ conftest.py
@@ -6,3 +6,24 @@ def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
+
+# function for parametrise all test functions
+def pytest_generate_tests(metafunc):  # metafunc - info about all fixtures
+    for fixture in metafunc.fixturenames:  # find your fixture
+        if fixture.startswith("exel_"):
+            testdata = load_from_exel(fixture[5:])  # load data from file(where name beginning after 5 symbols)
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])  # where, from, presentation
+
+
+# import testdata from module
+def load_from_exel(file):
+    groups = []
+    # path to file
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"data/{file}.xlsx")
+    xl = CreateObject("Excel.Application")
+    xl_book = xl.Workbooks.Open(file_path)
+    book_list = xl_book.Worksheets("Лист1")
+    for i in range(3):
+        groups.append(book_list.Range(f"A{i + 1}").Value[()])
+    xl.Quit()
+    return groups
